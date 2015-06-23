f3a0ef54fcedc8c8a0b2c4bd291360908fd213d0
jimtornado12 <jimtornado12@jimtornado12-PC>
2015-06-22 20:13:35 -0700

    My commit during class on June 22, 2015

---
+++ ITTMaster/src/com/itt/ifthenelse/IfThenElse.java
@@ -0,0 +1,15 @@
+package com.itt.ifthenelse;
+
+public class IfThenElse {
+	static int x=30;
+
+	public static void main(String[] args) {
+		if (x<20){
+			System.out.println("Yes, X is less than 20");
+		}
+		else{
+			System.out.println("No, X is not less than 20");
+		}
+	}
+
+}

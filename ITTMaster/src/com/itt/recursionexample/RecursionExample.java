b8f43581a587cf909d795acaa1457d8f62458bc9
jimtornado12 <jimtornado12@jimtornado12-PC>
2015-06-22 21:53:31 -0700

    Last Project of 062215

---
+++ ITTMaster/src/com/itt/recursionexample/RecursionExample.java
@@ -0,0 +1,23 @@
+package com.itt.recursionexample;
+
+public class RecursionExample {
+	private static int index = 0;
+	private static int stoppingPoint = 40;
+
+	public static void main(String[] args) {
+		int n1 = 0;
+		int n2 = 1;
+		System.out.println("index = " + index + "-> " + n1);
+		fibonacciSequence(n1, n2);
+		
+	}
+
+	public static void fibonacciSequence(int n1, int n2) {
+		System.out.println("index = " + index + "-> " + n2);
+		if (index == stoppingPoint)
+			return;
+		index++;
+		fibonacciSequence (n2, n1 + n2);
+		
+	}
+}
